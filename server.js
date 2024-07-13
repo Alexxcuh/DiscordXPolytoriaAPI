71c8ec0d69070430ea0bfed024d7593d9cab9850
Glitch (glitch-hello-node) <none>
2024-07-13 15:41:08 +0000

    🌒🌦 Checkpoint ./server.js:33677215/4751

--- server.js
+++ server.js
@@ -14,9 +14,45 @@ let latestMessage = '';
 client.on('messageCreate', message => {
   if (message.channel.id === DISCORD_CHANNEL_ID && !message.author.bot) {
     const displayName = message.member ? message.member.displayName : message.author.username;
-    latestMessage = `[DISCORD] ${displayName}: ${message.content}`;
+    let censoredContent = message.content;
+
+    // Define an object of words to censor mapped to their replacements
+    const censorMap = {
+      // Words to censor and their replacements
+      'n[i1]gg[aeiou]': '******',              // nigga, n1gga, n!gga, etc.
+      'f[ua]ck': '****',                       // fuck, fack, fuk
+      'mf': '**',                              // mf
+      'sh[i1]t': '****',                       // shit, sh1t, etc.
+      'v[aeiou]g[i1]n[aeiou]': '*****',        // vagina, v@gina, etc.
+      'p[a@]nt[i1][e3]s?': '****',             // panti, panties, panty, pantie
+      'b[i1]tc?h?': '****',                    // bitch, b1tch, b1tch, etc.
+      's[u0]ck[e3]r': '******',                // sucker, sukker, etc.
+      'c[h1]ld\\s*por[n]?': '*****',           // child porn, cp, child pornography
+      'porn': '****',                          // porn
+      'penis': '*****',                        // penis
+      'bullsh[i1]t': '*******',                // bullshit, bullsh1t, etc.
+      'rectum': '******',                      // rectum
+      'bbc': '***',                            // bbc
+      'wbc': '***',                            // wbc
+      'bbd': '***',                            // bbd
+      'wbd': '***',                            // wbd
+      'daisies\\s*destruction': '****************', // daisies destruction
+      'p0rn': '****',                          // p0rn
+      'nlgger': '******',                      // nlgger
+      'nigg3r': '******'                       // nigg3r
+    };
+
+    // Replace censored words with asterisks of the same length as the original word
+    for (const [pattern, replacement] of Object.entries(censorMap)) {
+      const regex = new RegExp(pattern, 'gi');
+      censoredContent = censoredContent.replace(regex, replacement);
+    }
+
+    latestMessage = `[DISCORD] ${displayName}: ${censoredContent}`;
   }
 });
+
+
 
 app.get('/message', (req, res) => {
   res.json({ message: latestMessage });
